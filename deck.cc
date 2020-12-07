b4974f8fbaeb547bbae2fe709181524045ead197
dsmustat <dsmustat@uwaterloo.ca>
2020-12-06 20:31:19 -0500

    Changes to be committed: 	new file:   deck.cc

---
+++ deck.cc
@@ -0,0 +1,27 @@
+#include "deck.h"
+#include <fstream>
+using namespace std;
+
+Deck::Deck(string filename) {
+    
+}
+
+void Deck::shuffle() {
+
+}
+
+Card Deck::get_top() {
+
+}
+
+Card Deck::pop() {
+
+}
+
+void Deck::remove(Card card) {
+
+}
+
+void Deck::add(Card card) {
+
+}
