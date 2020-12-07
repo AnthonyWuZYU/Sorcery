65df2a38aef76b63f71cb2ed6ed4fe38dda23b3b
k24kapoo <k24kapoo@student.cs.uwaterloo.ca>
2020-12-06 22:05:29 -0500

    player.cc added. player.h modified.

--- player.h
+++ player.h
@@ -15,15 +15,18 @@ Hand *hand;
 
 public:
 Player(); // Player default constructor
+Player( std::string name ); // Constructor that initializes using player's name
 virtual ~Player(); // Player destructor
 int getLife(); // method that returns player's life
-void setLife(); // method to change player's life
+void setLife( int life ); // method to change player's life
 int getMagic(); // method that returns player's magic value
-int setMagic(); // method to change player's magic value
+void setMagic( int magic ); // method to change player's magic value
 std::string getName(); // method that returns player's name
-void setName(); // method to change player's name
+void setName( std::string name ); // method to change player's name
 Deck getDeck(); // method that returns player's deck
 Hand getHand(); // method that returns player's hand
+void setDeck( Deck *deck ); // method to set/change the player's deck
+void setHand( Hand *hand ); // method to set/change the player's hand
 };
 
 #endif
