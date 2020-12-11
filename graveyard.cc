fe7d2561de67c4b521934336845d998266620e99
dsmustat <dsmustat@uwaterloo.ca>
2020-12-11 00:19:55 -0500

    fixed a few issues

--- graveyard.cc
+++ graveyard.cc
@@ -1,15 +1,15 @@
 #include "graveyard.h"
 
 Card Graveyard::revive() const{
-    Card temp = grave.back;
+    Card temp = grave.back();
 
-    grave.pop_back;
+    grave.pop_back();
 
     return temp;
 }
 
 Card Graveyard::top() const{
-    return grave.back;
+    return grave.back();
 }
 
 void Graveyard::add( Card card ){
