2d567020666e7fc99a5709fec79e66119ebf3eea
dsmustat <dsmustat@uwaterloo.ca>
2020-12-11 14:25:41 -0500

    added csv files

--- ability.cc
+++ ability.cc
@@ -1,4 +1,11 @@
 #include "ability.h"
 using namespace std;
 
+void Ability::activate_ability() {
+    
+}
+
+AbilityType Ability::get_type(){return type;}
+
+std::string Ability::get_description() {return description;}
 
