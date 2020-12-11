12fb33018d9b7ecd2063d5fb5ec325deb09e5513
k24kapoo <k24kapoo@student.cs.uwaterloo.ca>
2020-12-11 00:10:55 -0500

    graveyard implemented

---
+++ graveyard.h
@@ -0,0 +1,19 @@
+#ifndef GRAVEYARD_H
+#define GRAVEYARD_H
+#include <iostream>
+#include <vector>
+#include "card.h"
+
+class Graveyard{
+    std::vector<Card> grave; // The Vector that stores the dead cards of a player
+
+    public:
+
+    Card revive() const; // returns the top card ( most recent dead card ) and removes it from the vector grave
+
+    Card top() const; // returns the top card ( most recent dead card ) from the vector grave
+
+    void add( Card card ); // adds a dead card to the vector grave
+};
+
+#endif
