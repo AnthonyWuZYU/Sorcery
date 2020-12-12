452f1ad9d94596aea691edfcec182a02a34a1520
dsmustat <dsmustat@uwaterloo.ca>
2020-12-12 00:58:26 -0500

    updated board.h

--- board.h
+++ board.h
@@ -21,6 +21,8 @@ class Board {
         Card draw_from_deck();
         Card remove_from_field(unsigned int i);
         Card remove_from_hand(unsigned int i);
+        Card get_card_field(unsigned int i);
+        void set_card_field(unsigned int i);
         void move_to_graveyard(Card card);
         void move_to_hand(Card card);
 
