b10c61e80b85615c66f3bf21020998bc0ae75e1a
k24kapoo <k24kapoo@student.cs.uwaterloo.ca>
2020-12-13 02:00:38 -0500

    ritual.h and ritual.cc added

---
+++ ritual.cc
@@ -0,0 +1,41 @@
+#include "ritual.h"
+using namespace std;
+
+Ritual::Ritual( std::string name, int cost, int activation_cost, int charges, std::string card_type, std::string ability = "" ) :
+activation_cost{activation_cost}, charges{charges}, Card{name, cost, card_type} {}
+
+Ritual::~Ritual() {}
+
+Ritual::Ritual(const Card &other) : Card{other.get_Name(), other.get_Cost(), other.get_type()} {
+ const Ritual* temp = dynamic_cast<const Ritual*>(&other);
+ activation_cost = temp->getActivationCost();
+ charges = temp->getCharges();
+}
+
+int Ritual::getActivationCost() const{
+    return activation_cost;
+}
+
+int Ritual::getCharges() const{
+    return charges;
+}
+
+void Ritual::setCharges( int a ){
+    charges = a;
+}
+
+void Ritual::use_ability( Player *player, std::string description, Card *target ){
+        if (description == "At the start of your turn, gain 1 magic") {
+            player->setMagic(player->getMagic + 1);
+            
+            charges -= 1;  
+        } 
+        else if (desription == "1 | Whenever a minion enters play under your control, it gains +1/+1") {
+            //do something
+        }
+        else if (description == "Whenever a minion enters play, destroy it") {
+            //do something
+        }
+}
+
+
