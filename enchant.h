dca3cfdb959a0d96da94f53de278e075ad9ddace
Damian Mustatea <dsmustatea@uwaterloo.ca>
2020-12-13 01:11:44 -0500

    Update enchant.h

---
+++ enchant.h
@@ -0,0 +1,34 @@
+#ifndef ENCHANT_H
+#define ENCHANT_H
+#include "card.h"
+#include "player.h"
+#include <vector>
+
+class Player;
+
+class Enchant : public Card {
+    int addAtk;
+    int addDef;
+
+    public: 
+        // Constructor
+        Enchant(std::string name, int cost, int addAtk, int addDef, std::string ability="");
+
+        // Deconstructor
+        ~Enchant();
+
+        //Copy Assignment Operator
+
+        //use_ability(): uses the ability object of the card to perform the ability
+        //void use_ability() override;
+	
+	int get_addAtk();
+
+	int get_addDef();
+
+        //destroy(): destroys the card
+        void destroy() override;
+
+        void use_ability() override; 
+};
+#endif
