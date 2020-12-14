0418c1af3ea38a9da34238f63ae552a27a95ea80
Anthony Wu <anthony.wu@uwaterloo.ca>
2020-12-13 23:44:52 -0500

    Update enchant.h

--- enchant.h
+++ enchant.h
@@ -1,5 +1,6 @@
 #ifndef ENCHANT_H
 #define ENCHANT_H
+#include "ascii_graphics.h"
 #include "card.h"
 #include "player.h"
 #include <vector>
@@ -9,29 +10,38 @@ class Player;
 class Enchant : public Card {
     int addAtk;
     int addDef;
+    int mulAtk;
+    int mulDef;
+    std::string ability;
 
-    public: 
+    public:
         // Constructor
-        Enchant(std::string name, int cost, int addAtk, int addDef, std::string ability="");
+        Enchant(std::string name, int cost, int addAtk, int addDef, int mulAtk, int mulDef, std::string ability="");
+
+        Enchant(const Card *other);
 
         // Deconstructor
         ~Enchant();
 
+
         //Copy Assignment Operator
-        Card * operator=(const Card* other) override;
+        Card & operator=(const Card* other) override;
 
-        //use_ability(): uses the ability object of the card to perform the ability
-        //void use_ability() override;
-	
-	int get_addAtk();
+        int get_addAtk() const;
 
-	int get_addDef();
+        int get_addDef() const;
+
+        int get_mulAtk() const;
+
+        int get_mulDef() const;
+        std::string get_ability() const;
 
         //destroy(): destroys the card
         void destroy() override;
 
-        void use_ability(); 
-        void print(std::ostream &os) const override; 
-};
+        void use_ability();
 
+        void print(std::ostream &os) const override;
+
+};
 #endif
