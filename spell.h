a5c476b3cd0576f4bfe3ae6fe8d153f0e300cb60
Anthony Wu <anthony.wu@uwaterloo.ca>
2020-12-13 23:12:19 -0500

    Update spell.h

--- spell.h
+++ spell.h
@@ -7,9 +7,10 @@
 class Player;
 
 class Spell : public Card {
+        std::string ability;
     public:
         // Constructor
-        Spell(std::string name, int cost, std::string ability="");
+        Spell(std::string name, int cost, std::string ability = "");
 
         Spell(const Card *other);
 
@@ -18,6 +19,8 @@ class Spell : public Card {
 
         //Copy Assignment Operator
         Card & operator=(const Card* other) override;
+
+        std::string get_ability() const;
 
         void destroy() override;
 
