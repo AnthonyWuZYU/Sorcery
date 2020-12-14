a6119933e442a2095d532877e141eb899b4f1479
z335wu <z335wu@student.cs.uwaterloo.ca>
2020-12-13 20:21:53 -0500

    spell

---
+++ spell.h
@@ -0,0 +1,22 @@
+#ifndef SPELL_H
+#define SPELL_H
+#include "card.h"
+#include "player.h"
+
+class Player;
+
+class Spell : public Card {
+    public:
+        // Constructor
+        Spell(std::string name, int cost, std::string ability="");
+
+        // Deconstructor
+        ~Spell();
+
+        //Copy Assignment Operator
+
+        void destroy() override;
+
+        void use_ability() override;
+};
+#endif
