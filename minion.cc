f923ba9245065132e21d1296cdc4ee413e72ac9a
Anthony Wu <anthony.wu@uwaterloo.ca>
2020-12-14 21:50:09 -0500

    Update minion.cc

--- minion.cc
+++ minion.cc
@@ -229,3 +229,7 @@ std::vector<Enchant*> Minion::get_enchant() {
         return minionEnchantments;
 }
 
+void Minion::set_enchant(std::vector<Enchant*> enchants) {
+        minionEnchantments = enchants;
+}
+
